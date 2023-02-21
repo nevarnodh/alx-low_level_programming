b874a014a74f677ee9faf8622e36a4be6eec3206
nevarnodh <nevillendonji@yahoo.com>
2023-02-21 13:13:36 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,23 +10,23 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar(',');
-_putchar(' ');
 _putchar('+');
+_putchar(' ');
+_putchar(',');
 return (1);
 }
 else if (n == 0)
 {
-_putchar(',');
-_putchar(' ');
 _putchar('0');
+_putchar(' ');
+_putchar(',');
 return (0);
 }
 else
 {
-_putchar(',');
-_putchar(' ');
 _putchar('-');
+_putchar(' ');
+_putchar(',');
 return (-1);
 }
 }
