bee34dbd73af1cd5d4b4ddfc112181b865b590d6
nevarnodh <nevillendonji@yahoo.com>
2023-02-27 12:03:12 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the xter c to stdout
+ * @c: the xter to print
+ * Return: on success 1
+ * On error, -1 is returned, and  errno is set appropriately
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
