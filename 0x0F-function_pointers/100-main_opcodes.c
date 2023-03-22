14478027b0c5d3f3b1bf54a0da27b69655012c37
nevarnodh <nevillendonji@yahoo.com>
2023-03-22 04:28:36 -0700

    4. Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,23 +1,37 @@
-#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * int_index - Function that searches the integer
- * @array: array of integers
- * @size: size of array
- * @cmp: function pointer
- * Return: index of first element that matches with 'cmp' or -1 if none found
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (success)
  */
+int main(int argc, char *argv[])
+{
+int bytes, i;
+char *arr;
+if (argc != 2)
+printf('Error\n');
+exit(1);
+}
+bytes = atoi(argv[1]);
+if (bytes < 0)
+{
+printf('Error\n');
+exit(2);
+}
+arr = (char *)main;
+for (i = 0; i < bytes; i++)
+{
+if (i == bytes - 1)
+{
+printf('%02hhx\n', arr[i]);
+break;
+}
+printf('%02hhx\n', arr[i]);
+}
+return (0);
 
-int int_index(int *array, int size, int (*cmp)(int))
-{
-int i;
-if (size < 1 || array == NULL || cmp == NULL)
-return (-1);
-for (i = 0; i < size; i++)
-{
-if (cmp(array[i]))
-return (i);
-}
-return (-1);
-}
 
